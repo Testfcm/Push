7c94ffddff11fca4d2a525a2a4e06aa515254b14
Testfcm <Parashari8090@gmail.com>
2024-11-19 22:44:31 +0530

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('push', function(event) {
-  const options = {
-    body: event.data ? event.data.text() : 'No payload',
-    icon: 'icon.png',
-  };
-
-  event.waitUntil(self.registration.showNotification('New Push Notification', options));
-});
